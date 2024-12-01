f4ac56022de79b814e6a858f83a3021623908b45
florian.mayeux <florian.mayeux@monlycee.net>
2024-12-01 08:01:46 +0100

    added a sorting implementation instead of using python's default library

--- 2024/day1/python/level1.py
+++ 2024/day1/python/level1.py
@@ -1,5 +1,23 @@
 list1,list2 = [],[]
 
+def quicksort(array):
+    if len(array) <= 1:
+        return array
+
+    pivot = array[0]
+    upper = []
+    lower = []
+    equal = []
+
+    for elt in array:
+        if elt < pivot:
+            lower.append(elt)
+        elif elt > pivot:
+            upper.append(elt)
+        else:
+            equal.append(elt)
+    
+    return quicksort(lower) + equal + quicksort(upper)
 
 with open("input.txt","r") as f:
     for line in f.readlines():
@@ -7,8 +25,8 @@ with open("input.txt","r") as f:
         list1.append(id1)
         list2.append(id2)
 total = 0
-list1.sort()
-list2.sort()
+list1 = quicksort(list1)#list1.sort()
+list2 = quicksort(list2) #list2.sort()
 
 for id1,id2 in zip(list1,list2):
     total += abs(id1-id2)
