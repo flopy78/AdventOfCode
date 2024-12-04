3516143e4411e1f51533e0656ac7ecc14e158fd6
florian.mayeux <florian.mayeux@monlycee.net>
2024-12-04 07:39:43 +0100

    day 4 level2 complete

---
+++ 2024/day4/python/level2.py
@@ -0,0 +1,21 @@
+with open("input.txt") as file:
+    grid = file.read().split("\n")
+
+h = len(grid)
+w = len(grid[0])
+
+n_xmas = 0
+
+diag = {"M","S"}
+
+for y in range(h):
+    for x in range(w):
+        if grid[y][x] == "A":
+            if x in range(1,w-1) and y in range(1,h-1):
+                diag1 = {grid[y-1][x-1],grid[y+1][x+1]}
+                diag2 = {grid[y-1][x+1],grid[y+1][x-1]}
+                if diag1 == diag and diag2 == diag:
+                    n_xmas += 1
+     
+print(n_xmas)
+
