12974dad9d8e44330a03244c9cad8f7fbf7150f1
florian.mayeux <florian.mayeux@monlycee.net>
2024-12-09 18:44:16 +0100

    boost level 1 day 9 + summary

--- 2024/day9/python/level1.py
+++ 2024/day9/python/level1.py
@@ -1,3 +1,7 @@
+import time
+
+start = time.time()
+
 with open("input.txt") as file:
     memory = file.read()
     disk = []
@@ -8,15 +12,22 @@ with open("input.txt") as file:
             _id += 1
         else:
             disk += [" "]*int(memory[i])
+blank_idx = 0
+#n_spaces = disk.count(" ")
+#i = 0
 while " " in disk:
-    print(disk.count(" "))
+    while disk[blank_idx] != " ":
+        blank_idx += 1
     if disk[-1] != " ":
-        disk[disk.index(" ")] = disk[-1]
+        disk[blank_idx] = disk[-1]
     del disk[-1]
+    #i += 1
+    #print(i,"/",n_spaces)
 checksum = 0
 for i in range(len(disk)):
     checksum += i*disk[i]
 print(checksum)
+print(time.time()-start,"s")
     
 
         
