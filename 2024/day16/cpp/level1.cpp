cb5ed04bcbb9f3bdff66eece1af84369ac822727
flopy78 <94478046+flopy78@users.noreply.github.com>
2024-12-22 12:24:15 +0100

    Update level1.cpp

--- 2024/day16/cpp/level1.cpp
+++ 2024/day16/cpp/level1.cpp
@@ -9,11 +9,23 @@
 using namespace std;
 
 
+
+
 struct Position {
     int x;
     int y;
 };
 
+template<>
+struct std::hash<Position> {
+    size_t operator()(const Position &value) const {
+        return 7 * value.x + 13 * value.y;
+    }
+};
+
+bool operator==(const Position & A, const Position & B) {
+    return (A.x == B.x) && (A.y == B.y);
+}
 
 struct ReindeerStep {
     Position position;
@@ -21,29 +33,21 @@ struct ReindeerStep {
     int score;
 };
 
+
 bool operator < (const ReindeerStep & A, const ReindeerStep & B) {
-    if (A.score == B.score) {
-        if (A.position.y == B.position.y) {
-            if (A.direction.second == B.direction.second) {
-                if (A.position.x == B.position.x) {
-                    if (A.direction.first == B.direction.first) {
-                        return false;
-                    } else {
-                        return A.direction.first < B.direction.first;
-                    }
-                } else {
-                    return A.position.x < B.position.x;
-                }
-            } else {
-                return A.direction.second < A.direction.first;
-            }
-        } else {
-            return A.position.y < B.position.y;
-        }
-    } else {
+    if (A.score != B.score) 
         return A.score < B.score;
-    }
-}
+    if (A.position.y != B.position.y) 
+        return A.position.y < B.position.y;
+    if (A.direction.second != B.direction.second)
+        return A.direction.second < B.direction.second;
+    if (A.position.x != B.position.x)
+        return A.position.x < B.position.x; 
+    if (A.direction.first != B.direction.first) 
+        return A.direction.first < B.direction.first;
+    return false;
+}                    
+
 
 pair<int,int> turn_clockwise(pair<int,int> direction) {
     return make_pair(-direction.second,direction.first);
@@ -54,17 +58,6 @@ pair<int,int> turn_counterclockwise(pair<int,int> direction) {
     return make_pair(direction.second,-direction.first);
 }
 
-ReindeerStep get_best_step(set<ReindeerStep> opened_set) {
-    int best_score = -1;
-    ReindeerStep best_step;
-    for (ReindeerStep step : opened_set) {
-        if (best_score == -1 || step.score < best_score) {
-            best_score = step.score;
-            best_step = step;
-        }
-    }
-    return best_step;
-}
 
 bool is_finished(set<ReindeerStep> opened_set,Position end) {
     for (ReindeerStep step : opened_set) {
@@ -75,51 +68,34 @@ bool is_finished(set<ReindeerStep> opened_set,Position end) {
     return false;
 }
 
-bool contains(set<ReindeerStep> closed_set, ReindeerStep step,bool rotate) {
-    for (ReindeerStep closed_pos : closed_set) {
-        int x = step.position.x;
-        int y = step.position.y;
-        if (rotate) {
-            x += step.direction.first;
-            y += step.direction.second;
-        }
-        if (x == closed_pos.position.x && y == closed_pos.position.y){ //&& step.direction == closed_pos.direction) {
-            return true;
-        }
-    }
-    return false;
-}
-
-
 int get_best_score(vector<string> maze, ReindeerStep start, Position end) {
     set<ReindeerStep> opened_set {start};
     set<ReindeerStep> new_set;
-    set<ReindeerStep> closed_set {};
+    unordered_set<Position> closed_set {};
 
     int n_loops = 0;
     
     while (!is_finished(opened_set,end)) {
         ReindeerStep explored_step = *opened_set.begin();
 
-        closed_set.insert(explored_step);
+        closed_set.insert(explored_step.position);
         opened_set.erase(opened_set.begin());
-        //opened_set.erase(opened_set.find(explored_step));
         auto[dx,dy] = explored_step.direction;
         ReindeerStep new_step = {{explored_step.position.x+dx,explored_step.position.y+dy},explored_step.direction,explored_step.score + 1};
         if (new_step.position.x == end.x && new_step.position.y == end.y) {
             return new_step.score;
         }
-        if (maze[new_step.position.y][new_step.position.x] != '#' && !contains(closed_set,new_step,false)) {
+        if (maze[new_step.position.y][new_step.position.x] != '#' && !closed_set.contains(new_step.position)) {
             opened_set.insert(new_step);
         }
         auto[dx_cw,dy_cw] = turn_clockwise(explored_step.direction);
         ReindeerStep new_step_cw = {{explored_step.position.x,explored_step.position.y},{dx_cw,dy_cw},explored_step.score + 1000};
-        if (maze[explored_step.position.y+dy_cw][explored_step.position.x+dx_cw] != '#' && !contains(closed_set,new_step_cw,true)) {
+        if (maze[explored_step.position.y+dy_cw][explored_step.position.x+dx_cw] != '#' && !closed_set.contains({new_step_cw.position.x+dx_cw,new_step_cw.position.y+dy_cw})) {
             opened_set.insert(new_step_cw);
         }
         auto[dx_ccw,dy_ccw] = turn_counterclockwise(explored_step.direction);
         ReindeerStep new_step_ccw = {{explored_step.position.x,explored_step.position.y},{dx_ccw,dy_ccw},explored_step.score + 1000};
-        if (maze[explored_step.position.y+dy_ccw][explored_step.position.x+dx_ccw] != '#' && !contains(closed_set,new_step_ccw,true)) {
+        if (maze[explored_step.position.y+dy_ccw][explored_step.position.x+dx_ccw] != '#' && !closed_set.contains({new_step_ccw.position.x+dx_ccw,new_step_ccw.position.y+dy_ccw})) {
             opened_set.insert(new_step_ccw);
         }
 
@@ -152,6 +128,6 @@ int main() {
         }
         y++;
     }
-
-     cout << get_best_score(maze,start,end) << endl;
+    cout << maze.size() << " " << maze[0].length() << endl;
+    cout << get_best_score(maze,start,end) << endl;
 }
